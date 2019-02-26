6d08cf9c3a251e3ea6a078aeb8a9cf0ecb8c591a
DESKTOP-1RMTMKB\gamaiidev-pc <studiogamaii@gmail.com>
2019-02-26 18:19:24 -0500

    app build

---
+++ precache-manifest.547c251f36b8b5280523bf290a11d045.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "f1babb310a2639f7e85a",
+    "url": "/static/js/main.f1babb31.chunk.js"
+  },
+  {
+    "revision": "bcde1f648009baaa5e5b",
+    "url": "/static/js/2.bcde1f64.chunk.js"
+  },
+  {
+    "revision": "f1babb310a2639f7e85a",
+    "url": "/static/css/main.2611afe4.chunk.css"
+  },
+  {
+    "revision": "bcde1f648009baaa5e5b",
+    "url": "/static/css/2.97b9acf8.chunk.css"
+  },
+  {
+    "revision": "3254da559cbf46eb8080981fd92aafc1",
+    "url": "/index.html"
+  }
+];
